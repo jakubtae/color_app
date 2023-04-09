d9be097a480d2cd8585d1e60dba4e6fb8ce13c05
jakubtae <jakub.tomczyk.2005@gmail.com>
2023-04-09 09:54:25 +0200

    Initial Setup

---
+++ server.js
@@ -0,0 +1,29 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, "public"))); //! static folder declaration
+
+app.set("view engine", "ejs");
+app.use(cookieParser());
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.json())
+app.use(bodyParser.urlencoded({ limit: "10mB", extended: false })); //! for FORMS USAGE
+
+const landingRouter = require("./routes/landing");
+app.use("/", landingRouter);
+
+const randomcolorRouter = require("./routes/randomcolor");
+app.use("/randomcolor", randomcolorRouter);
+
+
+
+
+
+app.listen(3000);
