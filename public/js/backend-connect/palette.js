a7253604d568afec906f75a90d1c9785f51af083
jakubtae <jakub.tomczyk.2005@gmail.com>
2023-05-02 13:33:45 +0200

    Prawie Paleta

--- public/js/backend-connect/palette.js
+++ public/js/backend-connect/palette.js
@@ -1,3 +1,6 @@
+const footer = document.querySelector("footer");
+footer.classList.add("sticked");
+
 function hexCheck() {
   var hexinput = document.getElementById("hex_input");
   var hexcolor = document.getElementById("hex_input").value;
@@ -15,17 +18,41 @@ function hexCheck() {
   }
 }
 function fetchBetterPalette(){
+  console.log("Fetching")
   var hexcolor = document.getElementById("hex_input").value;
-  console.log(hexcolor);
   var xj = new XMLHttpRequest();
   xj.open("POST", "http://localhost:3000/palette", true);
   xj.setRequestHeader("Content-Type", "application/json");
   xj.send(JSON.stringify({ hexcolor:  hexcolor}));
   xj.onreadystatechange = async function () {
     if (xj.readyState == 4) {
-      var response = xj.responseText;
-      console.log(JSON.parse(response))
+      var response = JSON.parse(xj.responseText);
+      const content = document.getElementById("content");
+      content.innerHTML = response.html;
+      footer.classList.remove("sticked");
+
+      const cbox = document.querySelectorAll(".color-box");
+      
+      const msg = document.createElement("div");
+      msg.style.display = "block";
+      msg.style.position = "absolute";
+      msg.style.minWidth = "100px";
+      msg.style.aspectRatio = "1/1";
+      msg.style.background = "red";
+      function tellPos(p){
+      content.appendChild(msg);
+        msg.style.left = p.pageX+"px";
+        msg.style.top = p.pageY+"px";
+      }
+      
+       for (let i = 0; i < cbox.length; i++) {
+            cbox[i].addEventListener("mouseover mousemove", tellPos, false)
+       }
+
     }
   };
 }
 
+function justCopy(value){
+  navigator.clipboard.writeText(value);
+}
--- routes/palette.js
+++ routes/palette.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const HexToHsl = require("../models/HexToHsl.js");
 const fs = require('fs');
-functions = [];
-
+var functionNames = [];
+let ejs = require('ejs');
 //! IMPORTANT 
 //* DYNAMICLY READS THE MODELS DIRECTORY
 fs.readdirSync('./models/palette').forEach(function(file) {
     var name = file.replace('.js', '');
-    functions.push(name);
+    functionNames.push(name);
     exports[name] = require('../models/palette/' + file);
   });
 //! IMPORTANT 
@@ -19,16 +19,37 @@ router.get('/', (req, res) => {
 
 //! IMPORTANT 
 //* DYNAMICLY EXECUTE EACH EXPORTED FUNCTION FROM EARLIER READ DIRECTORY
-router.post('/', (req, res) => {
+// router.post('/', (req, res) => {
+//     var functionResults = [];
+//     for (var i = 0; i < functionNames.length; ++i) {
+//         //* exports is an object and by using object.values(exports)[i] we can use a function assinged to i value
+//         const functionWTF = {
+//             "name" : functionNames[i],
+//             "value" : Object.values(exports)[i](HexToHsl(req.body.hexcolor))
+//         }
+//         functionResults.push(functionWTF)
+//         //! pushes the result of function to arr
+//     }
+//     const maincolor = req.body.hexcolor;
+//     res.send({mainColor: maincolor, functions:functionResults});
+// })
+
+router.post('/', async (req, res) => {
     var functionResults = [];
-    for (var i = 0; i < functions.length; ++i) {
+    for (var i = 0; i < functionNames.length; ++i) {
         //* exports is an object and by using object.values(exports)[i] we can use a function assinged to i value
-        functionResults.push(Object.values(exports)[i](HexToHsl(req.body.hexcolor)))
+        const functionWTF = {
+            "name" : functionNames[i],
+            "values" : Object.values(exports)[i](HexToHsl(req.body.hexcolor))
+        }
+        functionResults.push(functionWTF)
         //! pushes the result of function to arr
     }
     const maincolor = req.body.hexcolor;
-    res.send({mainColor: maincolor,  function:functionResults});
+    const html = await ejs.renderFile('./views/updates/palette.ejs',{mainColor: maincolor, functions:functionResults});
+    res.send({html:html});
 })
+
 //! IMPORTANT 
 
 module.exports = router;
